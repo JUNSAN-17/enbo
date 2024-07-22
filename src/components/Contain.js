1a7531da76199ef2aa615d2b7083ee5ed99f43ea
Junsan <laugousing@163.com>
2024-07-22 21:56:13 +0800

    7.22_night_revision

--- src/components/Contain.js
+++ src/components/Contain.js
@@ -1,14 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
-    DesktopOutlined,
-    FileOutlined,
+    DatabaseOutlined,
+    AlertOutlined,
+    FireOutlined,
     PieChartOutlined,
-    TeamOutlined,
-    UserOutlined,
+    EnvironmentOutlined,
+    SunOutlined,
+    OpenAIOutlined,
+    FileDoneOutlined,
+    SettingOutlined
 } from '@ant-design/icons';
-import {Layout, Menu, theme, ConfigProvider} from 'antd';
+import { Layout, Menu, theme, ConfigProvider } from 'antd';
 
-const {Header, Content, Footer, Sider} = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
 function getItem(label, key, icon, children) {
     return {
@@ -20,82 +24,112 @@ function getItem(label, key, icon, children) {
 }
 
 const items = [
-    getItem('监控设备', '1', <PieChartOutlined/>,[
+    getItem('监控设备', '1', <span><i class="ri-camera-line"></i></span>, [
         getItem('设备状态', '2'),
         getItem('设备分组', '3'),
-    ]),
+    ])
 ];
+const items2 = [
+    getItem('地图显示', '4', <span><i class="ri-earth-line"></i></span>),
+    getItem('监控设备管理', '5', <span><i class="ri-camera-line"></i></span>),
+    getItem('防火资源管理', '6', <DatabaseOutlined />),
+    getItem('报警管理', '7', <AlertOutlined />),
+    getItem('历史火情', '8', <FireOutlined />),
+    getItem('巡护管理', '9', <EnvironmentOutlined />),
+    getItem('实景天气', '10', <SunOutlined />),
+    getItem('考核管理', '11', <FileDoneOutlined />),
+    getItem('算法调度管理', '12', <span><i class="ri-reactjs-fill"></i></span>),
+    getItem('系统管理', '13', <SettingOutlined />),
+]
+
 
 
 const Contain = () => {
     const [collapsed, setCollapsed] = useState(false);
     const {
-        token: {colorBgContainer, borderRadiusLG,},
+        token: { colorBgContainer, borderRadiusLG, },
     } = theme.useToken();
 
     return (
-        <Layout
-            style={{
-                minHeight: '100vh',
+        <ConfigProvider
+            theme={{
+                components: {
+                    Menu: {
+                        // darkItemColor:"#bfa",
+                        itemColor: "#fff",
+                        itemSelectedColor: "#1890ff",
+                        itemBorderRadius: 0,
+                        darkItemSelectedBg: "#1890ff"
+
+                    }
+                }
+
             }}
         >
-            <Header
+            <Layout
                 style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    backgroundColor: '#333b46',
+                    minHeight: '100vh',
                 }}
             >
-                <div className="demo-logo" style={{color: "#fff" ,fontSize: '17px'}}>
-                        恩博森林资源管理平台
-                </div>
-                <Menu
-                    mode="horizontal"
-                    defaultSelectedKeys={['2']}
-                    items={items}
+                <Header
                     style={{
+                        display: 'flex',
+                        alignItems: 'center',
                         backgroundColor: '#333b46',
-                        flex: 1,
-                        minWidth: 0,
-                    }}
-
-                />
-            </Header>
-            <Layout>
-                <Sider
-                    style={{backgroundColor: '#3c4551', color: 'white',}}
-                    collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                    {/*<div className="demo-logo-vertical"/>*/}
-                    <Menu
-                        theme="dark"
-                        defaultSelectedKeys={['1']}
-                        mode="inline"
-                        items={items}
-                        style={{
-                            backgroundColor: '#3c4551',
-                        }}
-                    />
-                </Sider>
-                <Content
-                    style={{
-                        margin: '10px 10px',
                     }}
                 >
-                    <div
+                    <div className="demo-logo" style={{ color: "#fff", fontSize: '18px' }}>
+                        <i class="ri-tree-fill"></i>
+                        恩博森林资源管理平台
+                    </div>
+                    <Menu
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        items={items2}
                         style={{
-                            padding: 24,
-                            minHeight: 360,
-                            height: '100%',
-                            background: colorBgContainer,
-                            borderRadius: borderRadiusLG,
+                            backgroundColor: '#333b46',
+                            flex: 1,
+                            minWidth: 0,
+                            fontSize:"15px"
+                        }}
+
+                    />
+                </Header>
+                <Layout>
+                    <Sider
+                        style={{ backgroundColor: '#3c4551', color: 'white' }}
+                        collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
+                    >
+                        <Menu
+                            theme="dark"
+                            defaultSelectedKeys={['1']}
+                            mode="inline"
+                            items={items}
+                            style={{
+                                backgroundColor: '#3c4551',
+                            }}
+                        />
+                    </Sider>
+                    <Content
+                        style={{
+                            margin: '10px 10px',
                         }}
                     >
-                        Bill is a cat.
-                    </div>
-                </Content>
-
+                        <div
+                            style={{
+                                padding: 24,
+                                minHeight: 360,
+                                height: '100%',
+                                background: colorBgContainer,
+                                borderRadius: borderRadiusLG,
+                            }}
+                        >
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                </Layout>
             </Layout>
-        </Layout>
+        </ConfigProvider>
     )
 }
 
